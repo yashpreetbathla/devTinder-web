22d6ac6627de5e445cfd8734aa1318e660abcb8a
yashpreetbathla <yashpreetbathla@gmail.com>
2025-01-18 11:50:42 +0530

    feat: signup page flow with login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,6 +10,12 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(false);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -35,11 +41,89 @@ const Login = () => {
     }
   };
 
+  const handleSignup = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        {
+          firstName,
+          lastName,
+          emailId,
+          password,
+          age,
+          gender,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      dispatch(addUser(res.data.data));
+      navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data?.message || "something went wrong");
+    }
+  };
+
   return (
     <div className="flex items-center justify-center my-20">
       <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login!</h2>
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Singup"}!
+          </h2>
+          {!isLoginForm && (
+            <>
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">First Name</span>
+                </div>
+                <input
+                  type="text"
+                  className="input input-bordered w-full max-w-xs"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </label>
+
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">Last Name</span>
+                </div>
+                <input
+                  type="text"
+                  className="input input-bordered w-full max-w-xs"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </label>
+
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">Age</span>
+                </div>
+                <input
+                  type="text"
+                  className="input input-bordered w-full max-w-xs"
+                  value={age}
+                  onChange={(e) => setAge(e.target.value)}
+                />
+              </label>
+
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">Gender</span>
+                </div>
+                <input
+                  type="text"
+                  className="input input-bordered w-full max-w-xs"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                />
+              </label>
+            </>
+          )}
+
           <label className="form-control w-full max-w-xs">
             <div className="label">
               <span className="label-text">Email ID</span>
@@ -64,10 +148,21 @@ const Login = () => {
           </label>
           <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center mt-2">
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+            <button
+              className="btn btn-primary"
+              onClick={isLoginForm ? handleLogin : handleSignup}
+            >
+              {isLoginForm ? "Login" : "Singup"}
             </button>
           </div>
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User? Signup Here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </div>
